059b2a5883983eedc088d365f37c2df599e0cd2a
Desmond Cheang <desmondcheang99@gmail.com>
2025-06-02 17:39:00 +1000

    Fixed title colour

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ function Navbar() {
         <div className="inset-x-0 top-0">
             <nav className="border-gray-200 bg-gray-900">
                 <div className="flex flex-wrap items-center justify-start mx-auto p-4">
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Leetcode Testcase Generator</span>
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Leetcode Testcase Generator</span>
                 </div>
             </nav>
         </div>
